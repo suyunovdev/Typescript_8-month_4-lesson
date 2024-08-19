d4f2a53a994608e1be260ac011c462221bf16757
suyunovdev <153995037+suyunovdev@users.noreply.github.com>
2024-08-19 13:12:48 +0500

    finished

--- src/app/productStore.ts
+++ src/app/productStore.ts
@@ -1,28 +1,45 @@
 import axios from "axios";
 import { create } from "zustand";
 
-export const useProductStore = create((set) => ({
+// Mahsulotlar uchun interfeys (Product interface)
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  images: string;
+}
+
+// Store interfeysi (ProductStore interface)
+interface ProductStore {
+  loading: boolean;
+  products: Product[];
+  error: string;
+  fetchProducts: () => Promise<void>;
+}
+
+// ProductStore uchun Zustand store'ini yaratish
+export const useProductStore = create<ProductStore>(set => ({
   loading: false,
   products: [],
   error: "",
   fetchProducts: async () => {
-    set((state) => ({
+    set(state => ({
       ...state,
       loading: true,
     }));
     try {
-      const res = await axios.get("http://localhost:3000/products");
-      const data = await res.data;
+      const res = await axios.get<Product[]>("http://localhost:3000/products");
       set(() => ({
         loading: false,
-        products: data,
+        products: res.data,
         error: "",
       }));
-    } catch (err) {
+    } catch (err: unknown) {
       set(() => ({
         loading: false,
         products: [],
-        error: err.message,
+        error: err instanceof Error ? err.message : "Something went wrong",
       }));
     }
   },
