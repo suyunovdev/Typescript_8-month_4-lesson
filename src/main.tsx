736b0b8df8fe48c179c4c7a2428c29d76387b505
suyunovdev <153995037+suyunovdev@users.noreply.github.com>
2024-08-19 13:34:37 +0500

    hommwork finish

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client"; // React 18 dan boshlab
+import App from "./App";
 import "./index.css";
+// root elementni olish
+const rootElement = document.getElementById("root");
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+if (rootElement) {
+  // root element mavjud bo'lsa, createRoot yordamida ildizni yaratish
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root element not found");
+}
